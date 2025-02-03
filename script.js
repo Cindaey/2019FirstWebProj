ef07af003f4e46b9c86a227293e8be0dc3c794b1
Cindyyy <98364607+Cindaey@users.noreply.github.com>
2025-02-03 12:37:45 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+//$(function () { 
+  //  $(window).scroll(function () {
+    //    if ($(this).scrollTop() > 5) { 
+      //      $('.logo').attr('src','images/smalllogo.svg').css({display:'inline'}).stop().animate({
+        //        height:'75px'},200);
+            //$('.store').css({display:'inline'}).stop().fadeIn(600);
+        //}
+    //    if ($(this).scrollTop() < 5) { 
+      //      $('.logo').attr('src','images/logo.png').stop().animate({
+          //      height:'170px'
+        //    },200);
+          // $('.store').stop().fadeOut("fast"); 
+        //}
+    //})
+//});
+function openNav() {
+    document.getElementById("theDrawer").style.right = "0rem";}
+function closeNav() {
+    document.getElementById("theDrawer").style.right= "-70rem";}
